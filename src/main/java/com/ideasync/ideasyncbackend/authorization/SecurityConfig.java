2c36f3f96762f390a10e2586787d6bdcefcdc056
Sean <91260510+seanwhitee@users.noreply.github.com>
2024-09-13 08:13:09 +0800

    Add update user endpoint and fix bugs (Issue: 32)

--- src/main/java/com/ideasync/ideasyncbackend/authorization/SecurityConfig.java
+++ src/main/java/com/ideasync/ideasyncbackend/authorization/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
@@ -24,13 +25,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity.authorizeHttpRequests(authorize ->
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorize ->
                         // Here we set authentication for all endpoints
-                        authorize.requestMatchers("/api/v1/users/sendEmail").permitAll()
-                                .requestMatchers("/api/v1/users/login").permitAll()
-                                .requestMatchers("/api/v1/users/saveUserData").permitAll()
-                                .requestMatchers("/api/v1/users/register").permitAll()
-                                .requestMatchers("/api/v1/authorization/generateToken").permitAll()
+                        authorize.requestMatchers("/api/v1/users/sendEmail",
+                                        "/api/v1/users/login",
+                                        "/api/v1/users/saveUserData",
+                                        "/api/v1/users/checkUserData",
+                                        "/api/v1/authorization/generateToken").permitAll()
                                 .anyRequest().authenticated()
                 )
                 // Here we enable that we will accept JWTs
